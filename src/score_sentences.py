65c9bdd255f7d661112b9506c83fd5dd6cb9d10c
anonymous <thclark@mit.edu>
2023-03-01 19:13:50 -0500

    fairseq embeddings and eval

--- src/score_sentences.py
+++ src/score_sentences.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         if custom_lm.encode(l).size(0) > custom_lm.max_positions - 2:
             l = " ".join(l.split()[: custom_lm.max_positions - 2])
         tokens = custom_lm.encode(l)
-        all_layers = custom_lm.models[0].extract_features(tokens)
+        all_layers = custom_lm.models[0].extract_features(tokens, encoder_out=None)
         all_embeddings.append(all_layers[0])
         print(all_layers[0].shape)
 
