5323a59ef771b007a3c571d6aea84c6531b579bf
anonymous <thclark@mit.edu>
2023-03-02 17:20:47 -0500

    sanity checks

--- src/score_sentences.py
+++ src/score_sentences.py
@@ -2,6 +2,21 @@ import sys
 import torch
 from fairseq.models.transformer_lm import TransformerLanguageModel
 import argparse
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import pandas as pd
+
+
+def make_plot(surprisals, tokens, plot_dir, imgname):
+    df = pd.DataFrame({"surprisal": surprisals, "token": tokens})
+    df.surprisal = -1 * df.surprisal
+    sns.axes_style("whitegrid")
+    fig, ax = plt.subplots(figsize=(16, 8))
+    sns.lineplot(data=df)
+    ax.set_xticks(range(len(df)), labels=list(df.word), ha="left", rotation=-45)
+    plt.savefig(os.path.join(plot_dir, imgname), dpi=150, bbox_inches="tight")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -9,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_dir")
     parser.add_argument("--test_file")
     parser.add_argument("--out_file")
+    parser.add_argument("--plot_dir")
     parser.add_argument("--emb_out_file")
     args = parser.parse_args()
 
@@ -19,12 +35,13 @@ if __name__ == "__main__":
     )
     custom_lm.eval()
 
+    # read data
     with open(args.test_file, "r") as f:
         lines = f.read().splitlines()
 
-    count = 0
+    # get per-token surprisals
     lprobs, perps, tokens = [], [], []
-    for l in lines:
+    for item, l in enumerate(lines):
         print(l)
         if custom_lm.encode(l).size(0) > custom_lm.max_positions - 2:
             l = " ".join(l.split()[: custom_lm.max_positions - 2])
@@ -33,10 +50,12 @@ if __name__ == "__main__":
         lprobs.append(out["positional_scores"])
         print(out["positional_scores"])
         tokens.append([custom_lm.tgt_dict[i] for i in out["tokens"]])
+        print(tokens[-1])
+        make_plot(lprobs[-1], tokens[-1], args.plot_dir, f"sanity_check_plot_{item}")
     torch.save([lprobs, tokens], args.out_file)
 
+    # get hidden representations
     all_embeddings = []
-    # hidden representations
     for l in lines:
         if custom_lm.encode(l).size(0) > custom_lm.max_positions - 2:
             l = " ".join(l.split()[: custom_lm.max_positions - 2])
@@ -48,6 +67,5 @@ if __name__ == "__main__":
         extracted_features = extra["inner_states"]
         all_embeddings.append(extracted_features)
         print("Final Hidden Layer Shape:", extracted_features[-1].shape)
-
     torch.save(all_embeddings, args.emb_out_file)
 
